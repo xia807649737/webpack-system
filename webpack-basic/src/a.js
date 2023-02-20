2a9d5e173353ba2ae74b347f4456287e0e333360
xiahong <807649737@qq.com>
2023-02-20 16:47:47 +0800

    webpack的多个文件打包，通过一个入口引入很多外部文件

---
+++ webpack-basic/src/a.js
@@ -0,0 +1,10 @@
+console.log('我是A模块');
+//CommonJS规范在浏览器中不支持
+// module.exports = {
+//     name: 'aaaa'
+// }
+
+//es6的导出规范
+export default {
+    name: 'bbb'
+}
--- webpack-basic/src/index.js
+++ webpack-basic/src/index.js
@@ -1,1 +1,7 @@
+//CommonJS规范 在浏览器中不支持
+// let a = require('./a.js');
+
+//es6导入规范
+import a from './a.js';
+console.log(a);
 console.log('今天晚上早点睡觉啦');
