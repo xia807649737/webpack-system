a3ca215c7e0175abf858f0ac7569f39a6f651a73
xiahong <807649737@qq.com>
2023-02-21 14:13:38 +0800

    wbpack修改指定打包文件

---
+++ webpack-basic/webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+// webpack的配置文件遵循CommonJS规范
+module.exports = {
+    entry: './src/index.js',  //程序的入口文件
+    output: {                 //文件打包后存入的位置
+        // path.resolve() : 解析当前相对路径的绝对路径
+        // path: path.resolve('./dist/'),
+        // path: path.resolve(__dirname,'./dist/'),
+        path: path.join(__dirname, './dist/'),
+        filename: 'main.js', //输出后的文件名
+    },
+    mode: 'development'   //默认为production
+    
+}
